eaea25a6c1d22fc0d89e1b222ebb1eb8fd546552
Kiri111enz <omelyanenkokirill@gmail.com>
2023-05-02 08:17:23 +0300

    initial commit

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from 'styles/Home.module.scss';
+
+const Home: React.FC = () => (
+    <div className={styles.container}>
+        Watch Together
+    </div>
+);
+
+export default Home;
