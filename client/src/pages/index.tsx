943dc6d0ad90f09dd45c50a0792036a5f93cfb4c
Kiri111enz <omelyanenkokirill@gmail.com>
2023-05-02 08:37:09 +0300

    home page main buttons

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,7 +2,8 @@ import styles from 'styles/Home.module.scss';
 
 const Home: React.FC = () => (
     <div className={styles.container}>
-        Watch Together
+        <button className={styles.buttonLarge}>Create room</button>
+        <button className={styles.buttonLarge}>Join room</button>
     </div>
 );
 
